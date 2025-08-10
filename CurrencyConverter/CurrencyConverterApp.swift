27bf31367bdd77929df567fe20c2d39a31dc6951
Ilya Suvorov <vsilux@gmail.com>
2025-08-10 19:04:51 +0300

    initial commit

---
+++ CurrencyConverter/CurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterApp.swift
+//  CurrencyConverter
+//
+//  Created by Illia Suvorov on 10.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
